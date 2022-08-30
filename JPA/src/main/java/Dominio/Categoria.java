d47b962a631629ecc6578cf190512be62a6d43db
Izaqui <izaquiel.silva.ads@gmail.com>
2022-08-30 14:19:31 -0300

    entidades

---
+++ JPA/src/main/java/Dominio/Categoria.java
@@ -0,0 +1,11 @@
+package Dominio;
+
+import java.util.Date;
+import java.util.List;
+
+public class Categoria {
+    private int id;
+    private String nome;
+    private Date dataCriacao;
+    private List<Produto> produtos;
+}
---
+++ JPA/src/main/java/Dominio/Produto.java
@@ -0,0 +1,12 @@
+package Dominio;
+
+import java.util.Date;
+import java.util.List;
+
+public class Produto {
+        private int id;
+        private String nome;
+        private String descricao;
+        private Date dataDeValidade;
+        private List<Categoria> categorias;
+}
